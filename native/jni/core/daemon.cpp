dfa463df918d5877b4813aaccd126ab3947dee43
LoveSy <shana@zju.edu.cn>
2021-12-31 18:13:59 +0800

    Update daemon.cpp

--- native/jni/core/daemon.cpp
+++ native/jni/core/daemon.cpp
@@ -397,10 +397,7 @@ int connect_daemon(bool create) {
             return -1;
         }
 
-        if (fork_dont_care() == 0) {
-            close(fd);
-            daemon_entry();
-        }
+        daemon_entry();
 
         while (connect(fd, (struct sockaddr*) &sun, len))
             usleep(10000);
